e8d960eed3e99368374f008d6d31d836b27604eb
Samuel A. Yallop <syallop@gmail.com>
2020-04-19 15:23:28 +0100

    Print first example input to page

--- JS/Main.hs
+++ JS/Main.hs
@@ -1,6 +1,6 @@
--- | Haskell language pragma
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 -- | Haskell module declaration
 module Main where
@@ -19,66 +19,126 @@ import qualified PL.Type as PL
 import PL.Type hiding (Type)
 
 import PLRepl.Repl
+import qualified PLRepl.Widgets.State as PL
+import PLRepl.Widgets.Name
+import PLRepl.Widgets.Event
+
+import qualified PL.Test.Expr as Test
+import qualified PL.Test.ExprTestCase as Test
+import qualified PLLispy.Test.Sources.Expr as Test
+
 import PL.Var
 import PL.TyVar
 import PL.FixExpr
 import PL.FixType
 
+import qualified PLEditor as E
+
 -- Other dependencies
 import Data.Text (Text)
 import qualified Data.Text as Text
+import Data.Map (Map)
+import qualified Data.Map as Map
 
+import System.Random
+
+-- Aliases for the concrete expression/ types we're going to use
 type Expr = PL.Expr Var Type TyVar
 type Type = PL.Type TyVar
 
-idNat :: Expr
-idNat = FixExpr $ Lam (FixType $ Named "Nat") (FixExpr $ Binding $ VZ)
+newtype State n = State (PL.State n)
 
--- | Type synonym for an application model
-data Model = Model
-  { output :: Text
-  }
-  deriving Eq
+-- TODO: Presumably is we defined some partial equality here we could avoid
+-- doing computations on identical states?
+instance Eq (State n) where
+ _ == _ = False
 
--- | Sum type for application events
-data Action
-  = NoOp
-  | PrintExpr Expr
-  deriving (Show, Eq)
-
--- | Entry point for a miso application
 main :: IO ()
-main = startApp App {..}
+main = run App{..}
   where
+    run :: Eq (State Name) => App (State Name) (Event Name) -> JSM ()
+    run = startApp
 
     -- Executed on application load
-    initialAction = PrintExpr idNat
-    model  = Model "EMPTY"
-    update = updateModel
-    view   = viewModel
+    initialAction :: Event Name
+    initialAction = EditorEv . InsertText . Test._parsesFrom . snd . (!! 0) . Map.toList $ exampleLispyTestCases
 
-    -- Delegated events
+    model :: State Name
+    model = State $ PL.initialState (Just EditorCursor) usage
+
+    update :: Event Name -> State Name -> Effect (Event Name) (State Name)
+    update = handleEvent
+
+    view :: State Name -> View (Event Name)
+    view = drawUI
+
+    -- Delegated events the body will listen for
+    events :: Map MisoString Bool
     events = defaultEvents
 
-    -- Subsriptions
-    subs   = []
+    -- Subscriptions ran during application lifetime
+    subs :: [Sink (Event Name) -> JSM ()]
+    subs = []
 
-    -- Nothing defaults to 'body'
+    -- Root element for DOM diff
+    mountPoint :: Maybe MisoString
     mountPoint = Nothing
 
--- | Updates model, optionally introduces side effects
-updateModel :: Action -> Model -> Effect Action Model
-updateModel a m = case a of
-  NoOp
-    -> noEff m
+-- | Transform the state in response to an event with optional side effects.
+handleEvent :: Event Name -> State Name -> Effect (Event Name) (State Name)
+handleEvent ev (State st) = case ev of
+  -- Replace the entire repl state
+  ReplaceReplState someReplState'
+   -> noEff $ State $ st{PL._replState = someReplState'}
 
-  PrintExpr expr
-    -> noEff (m{ output = Text.pack . show $ expr })
+  -- An event for the editor
+  EditorEv editorEv
+   -> noEff $ State $ st{PL._editorState = handleEditorEventDefault editorEv $ PL._editorState st}
 
--- | Constructs a virtual DOM from a model
-viewModel :: Model -> View Action
-viewModel x = div_ [] [
-   button_ [ onClick (PrintExpr idNat) ] [ text "Print expression" ]
- , text (ms . output $ x)
- ]
+  -- An event for the output
+  OutputEv outputEv
+   -> noEff $ State $ st{PL._outputState = handleOutputEventDefault outputEv $ PL._outputState st}
+
+  -- An event for the type context
+  TypeCtxEv typeCtxEv
+   -> noEff $ State $ st{PL._typeCtxState = handleTypeCtxEventDefault typeCtxEv $ PL._typeCtxState st}
+
+  -- An event for the usage
+  UsageEv usageEv
+   -> noEff $ State $ st{PL._usageState = handleUsageEventDefault usageEv $ PL._usageState st}
+
+  -- Switch input focus
+  FocusOn n
+   -> noEff $ State $ st{PL._focusOn = n}
+
+-- Draw the entire state
+drawUI :: State Name -> View (Event Name)
+drawUI (State st) = div_ [] $ [
+  text "Input:"
+  ] <> [drawEditor EditorCursor (PL._editorState st)]
+  where
+
+  drawEditor
+    :: Name
+    -> PL.EditorState
+    -> View (Event Name)
+  drawEditor editorCursor (PL.EditorState editor view) = div_ [] [ text . ms . (\(lines,pos) -> Text.unlines . fmap E.lineText . E.renderLines $ lines) . E.viewEditor view $ editor ]
+
+usage :: [Text]
+usage =
+  ["Usage"
+  ]
+
+-- Generate random example from the Lispy implementation of the PL test cases
+randomExample :: IO Text
+randomExample = do
+  let exampleNames = Map.keys exampleLispyTestCases
+      nExamples    = Prelude.length exampleNames
+  randomIndex <- randomRIO (0,nExamples - 1)
+  let randomName = exampleNames !! randomIndex
+      Just randomTestCase = Map.lookup randomName exampleLispyTestCases
+  return . Test._parsesFrom $ randomTestCase
+
+exampleLispyTestCases :: Map Text Test.ExprTestCase
+exampleLispyTestCases = Map.fromList $ Test.testCases Test.sources
 
