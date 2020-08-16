314fbe37d7f0049fa9b65b649abf6a56c62a0995
Samuel A. Yallop <syallop@gmail.com>
2020-08-16 13:49:53 +0100

    Teach cli how to lookup type and kind hashes

--- CLI/Main.hs
+++ CLI/Main.hs
@@ -75,7 +75,14 @@ data Command
   -- | Execute the TUI repl.
   | TerminalREPL
 
+  -- | Lookup an expression by it's hash
   | LookupExpr String
+
+  -- | Lookup a type by it's hash
+  | LookupType String
+
+  -- | Lookup a kind by it's hash
+  | LookupKind String
   deriving Show
 
 -- | Read and parse command line options into a Command.
@@ -90,11 +97,19 @@ parseCommand = customExecParser (prefs showHelpOnError) commandParserInfo
     mkCommand (name, desc, parser) = command name $ info parser (progDesc desc)
 
     commandParser :: O.Parser Command
-    commandParser = hsubparser . mconcat . fmap mkCommand $ [terminalRepl, version, lookupExpr]
+    commandParser = hsubparser . mconcat . fmap mkCommand $ [terminalRepl, version, lookup]
 
     terminalRepl = ("repl"   , "start a repl to Read, Evaluate, Print (Loop) code", pure TerminalREPL)
     version      = ("version", "show the version of PL being used",                 pure ShowVersion)
-    lookupExpr   = ("lookup",  "lookup the expression associated with a hash",      LookupExpr <$> (strArgument $ mconcat [help "Expression hash", metavar "EXPR_HASH"]))
+    lookup       = ("lookup",  "lookup something associated with a hash",           hsubparser . mconcat . fmap mkCommand $
+                                                                                      [ ("expr", "lookup an expression associated with a hash", lookupExpr)
+                                                                                      , ("type", "lookup a type associated with a hash"       , lookupType)
+                                                                                      , ("kind", "lookup a kind associated with a hash"       , lookupKind)
+                                                                                      ])
+
+    lookupExpr = LookupExpr <$> (strArgument $ mconcat [help "Expression hash", metavar "EXPR_HASH"])
+    lookupType = LookupType <$> (strArgument $ mconcat [help "Type hash", metavar "TYPE_HASH"])
+    lookupKind = LookupKind <$> (strArgument $ mconcat [help "Kind hash", metavar "KIND_HASH"])
 
 runCommand :: Command -> IO ()
 runCommand cmd = case cmd of
@@ -127,6 +142,51 @@ runCommand cmd = case cmd of
                    Just expr
                      -> Text.putStrLn . PLPrinter.render . Lispy.ppExpr $ expr
 
+  LookupType shortHashString
+    -> do (_replCtx, log, eRes) <- runRepl replCtx $ do
+            -- Assume hashes are provided in Lispy syntax
+            shortHash <- plGrammarParser Lispy.shortHash $ Text.pack shortHashString
+            hash <- replResolveTypeHash shortHash
+            replLookupType hash
+          case eRes of
+            Left err
+              -> do Text.putStrLn . PLPrinter.render . mconcat $
+                      [ ppError Lispy.ppDefaultError $ err
+                      , lineBreak
+                      , log
+                      ]
+                    exitFailure
+
+            Right mType
+              -> case mType of
+                   Nothing
+                     -> putStrLn "There are no known types with the given hash"
+
+                   Just typ
+                     -> Text.putStrLn . PLPrinter.render . Lispy.ppType $ typ
+
+  LookupKind shortHashString
+    -> do (_replCtx, log, eRes) <- runRepl replCtx $ do
+            -- Assume hashes are provided in Lispy syntax
+            shortHash <- plGrammarParser Lispy.shortHash $ Text.pack shortHashString
+            hash <- replResolveKindHash shortHash
+            replLookupKind hash
+          case eRes of
+            Left err
+              -> do Text.putStrLn . PLPrinter.render . mconcat $
+                      [ ppError Lispy.ppDefaultError $ err
+                      , lineBreak
+                      , log
+                      ]
+                    exitFailure
+
+            Right mKind
+              -> case mKind of
+                   Nothing
+                     -> putStrLn "There are no known kinds with the given hash"
+
+                   Just kind
+                     -> Text.putStrLn . PLPrinter.render . Lispy.ppKind $ kind
   where
     -- Build a replctx by hijacking the codestore used in the TUI.
     -- TODO: TUI should accept a codestore as an argument/ this logic belongs in
